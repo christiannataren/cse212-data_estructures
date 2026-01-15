19bc70c18bb34b633bcd80d5260c064b5f2ecdc7
christiannataren <cnatarnlaguna@byupathway.edu>
2026-01-14 23:00:47 -0500

    week1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Start creating a list where the results will be saved
+        //Loop as much times as indicated by the lenght + 1 and starting  the loopCounter from 1 and incrementing the loopcount by 1
+        //Add the result of the operation (number * loopCounter) in the list
+        ///After looping return the list as array
+
+        List<double> multi = new List<double>();
+        for (int i = 1; i < length + 1; i++)
+        {
+            multi.Add(number * i);
+        }
+
+        return multi.ToArray(); // replace this return statement with your own
     }
 
     /// <summary>
@@ -25,9 +38,28 @@ public static class Arrays
     /// </summary>
     public static void RotateListRight(List<int> data, int amount)
     {
+
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Start looping by the amount of numbers will be moving
+        //Save in a variable the last number, it alway be the total of the data size - 1
+        /// Make a copy of the data
+        ///Save the last number in the index 0 of the data
+        /// Loop the copy starting  on 0 and finishing when the loopCounter is less than the size - 1
+        /// Save each index of the copy in the data file using as index the loopCounter + 1
+
+        for (int i = 0; i < amount; i++)
+        {
+            int loss = data[data.Count - 1];
+            int[] copy = data.ToArray();
+            data[0] = loss;
+            for (int e = 0; e < copy.Length - 1; e++)
+            {
+                data[e + 1] = copy[e];
+            }
+        }
     }
 }
--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -3,14 +3,33 @@ public static class ArraySelector
     public static void Run()
     {
         var l1 = new[] { 1, 2, 3, 4, 5 };
-        var l2 = new[] { 2, 4, 6, 8, 10};
-        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1};
+        var l2 = new[] { 2, 4, 6, 8, 10 };
+        var select = new[] { 1, 1, 1, 2, 2, 1, 2, 2, 2, 1 };
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        List<int> list = new List<int>();
+        int taken1 = 0;
+        int taken2 = 0;
+        for (int i = 0; i < select.Length; i++)
+        {
+            int selected;
+            if (select[i] == 1)
+            {
+                selected = list1[taken1];
+                taken1 += 1;
+            }
+            else
+            {
+                selected = list2[taken2];
+                taken2 += 1;
+            }
+            list.Add(selected);
+        }
+
+        return list.ToArray();
     }
 }
